3a76d440288e0a80aa20653480698f4be4a95302
rmonier <romain.monier0609@gmail.com>
2022-11-09 23:39:45 +0100

    Edited analysis values

--- evaluation/analysis.py
+++ evaluation/analysis.py
@@ -2,8 +2,8 @@ import subprocess
 
 def main():
     activations = ["sigmoid"]
-    learning_rates = [0.1, 0.5, 1.0, 1.5, 2.0]
-    momentums = [0.1, 0.5, 0.9, 1.5, 2.0]
+    learning_rates = [0.01, 0.05, 0.1, 0.15, 0.2]
+    momentums = [0, 0.1, 0.5, 0.7, 0.9]
     epochs = [10, 100, 200, 300, 400, 500, 1000]
     hidden_layers = [[9,5], [4,6], [9,5,6], [4,6,8], [2,3]]
     for activation in activations:
